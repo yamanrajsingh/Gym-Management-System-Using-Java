b5c8dd9d4987b93e4674aed080cdb308dbd8a456
Yaman Raj Singh <76215280+yamanrajsingh@users.noreply.github.com>
2023-09-30 15:07:05 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/fitforyuva/FitForYuva.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fitforyuva;
+
+/**
+ *
+ * @author hp
+ */
+public class FitForYuva {
+
+    public static void main(String[] args) {
+    }
+}
